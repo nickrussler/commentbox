075379ce4c31b08b08b4211b89ee8820ccf85477
Nick Russler <nick.russler@gmail.com>
2013-02-24 02:26:33 +0100

    Split project into a jar file and a demo project.

--- source/src/db/eao/local/CommentReportEAOLocal.java
+++
@@ -1,7 +0,0 @@
-package db.eao.local;
-
-import db.entity.CommentReport;
-
-public interface CommentReportEAOLocal extends BaseEAOLocal<CommentReport> {
-
-}
--- source/src/db/enums/ImageURLValidateResult.java
+++
@@ -1,5 +0,0 @@
-package db.enums;
-
-public enum ImageURLValidateResult {
-	ValidJPEG, ValidPNG, ValidGIF, UnkownError, NoHTTPConnectionError, NoImageContentTypeError, MaxImageSizeExceededError, NoLengthError
-}
