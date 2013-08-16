f1436ab2ebeaa98ecd84f1c54c3df7acbff3c9c2
nick_russler <nick.russler@gmail.com>
2013-08-16 21:13:14 +0200

    Mark extended Demo Folder as Deprecated

--- extended-demo-source/src/db/eao/local/ArticleReportEAOLocal.java
+++
@@ -1,7 +0,0 @@
-package db.eao.local;
-
-import db.entity.ArticleReport;
-
-public interface ArticleReportEAOLocal extends BaseEAOLocal<ArticleReport> {
-
-}
--- extended-demo-source/src/db/eao/local/UserEAOLocal.java
+++
@@ -1,7 +0,0 @@
-package db.eao.local;
-
-import db.entity.User;
-
-public interface UserEAOLocal extends BaseEAOLocal<User> {
-	public User findUserByUsername(String username);
-}
